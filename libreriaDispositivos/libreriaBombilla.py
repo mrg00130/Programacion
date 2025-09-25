512518bd9f6dded90ea5d7b252c8f38c8649f9fb
Lolo <mrg00130@red.ujaen.es>
2025-09-25 10:07:03 +0200

    primera version de prorgramacion definicion de una bombilla

---
+++ libreriaDispositivos/libreriaBombilla.py
@@ -0,0 +1,8 @@
+def creaBombilla (estado = False, intensidad = 100, color = (0, 0, 255)):
+
+    bombilla = {
+        'estado' : estado,
+        'intensidad' : intensidad,
+        'color' : color
+    }
+    return bombilla
---
+++ principal.py
@@ -0,0 +1,5 @@
+from libreriaDispositivos.libreriaBombilla import creaBombilla
+
+bomb1 = creaBombilla(2)
+
+print(bomb1)
