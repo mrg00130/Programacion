6c3da49282b2b25b54887104d7c2bdb8fe11b781
Lolo <mrg00130@red.ujaen.es>
2025-10-01 13:02:02 +0200

    primera version de prorgramacion definicion de una bombilla

--- libreriaDispositivos/libreriaBombilla.py
+++ libreriaDispositivos/libreriaBombilla.py
@@ -1,8 +1,13 @@
-def creaBombilla (estado = False, intensidad = 100, color = (0, 0, 255)):
-
-    bombilla = {
-        'estado' : estado,
-        'intensidad' : intensidad,
-        'color' : color
-    }
+def creaBombilla(estado =True, nivelIntensidad = 100, color = (255,255,255)):
+    bombilla = {"estado": estado, "nivelIntensidad": nivelIntensidad, "color": color}
     return bombilla
+
+def imprimirBombilla(bombilla):
+    print("---------------------------------------")
+    if (bombilla["estado"]):
+        print("Bombilla encendida")
+    else:
+        print("Bombilla apagada")
+    print("Nivel de intensidad:", bombilla["nivelIntensidad"])
+    print("Color:", bombilla["color"])
+    print("---------------------------------------")
--- principal.py
+++ principal.py
@@ -1,5 +1,10 @@
 from libreriaDispositivos.libreriaBombilla import creaBombilla
-
+from libreriaDispositivos import  libreriaAcondicionado as liba
 bomb1 = creaBombilla(2)
+aire1= liba.aireAcondicionado()
+liba.imprimirAire(aire1)
+liba.apagarAire(aire1)
+liba.imprimirAire(aire1)
+liba.cambiarTemperatura(aire1)
 
-print(bomb1)
+
