cf41f9a3f51ac67d2b797c9cdf28849fbf8b6a14
aulas <mrg00130@red.ujaen.es>
2025-10-06 19:16:55 +0200

    he implementado el aparatdo de temporizadores

--- principal.py
+++ principal.py
@@ -4,20 +4,30 @@ from libreriaDispositivos.libreriaTemporizadores import *
 from libreriaDispositivos import  libreriaHub as hub
 from libreriaDispositivos import  libreriaHabitacion as hab
 
+while(1):
+    print("---------------------------------------")
+    print("----------------MENU--------------------")
+    print("---------------------------------------")
 
-hogar = hub.crearCasa()
-
-listaHab = list()
-aire1=creaAireAcondicionado("Aire del salon")
-salon = hab.creaHabitacion("salon")
-candelabro = creaBombilla("candelabro")
-bomb2 = creaBombilla()
-hab.anadeBombillaHabitacion(salon, candelabro)
-hab.anadeAireHabitacion(salon, aire1)
-hab.imprimeHabitacion(salon)
+'''1. HU01: Gestión de una bombilla inteligente
+Se puede encender y se puede apagar
+Saber su estado
+Cambiar la intensidad
+Cambiar el color'''
 
 
-temporizadorAireEncender(aire1,1)
-hab.imprimeHabitacion(salon)
-temporizadorAireApagar(aire1,1)
-hab.imprimeHabitacion(salon)
+'''2. HU02: Gestióin del aire acondicionado
+Se debe poder conocer la temperatura del aire
+Se debe poder cambiar la temperatura
+Se debe poder conocer si está apagado o encendido
+Se debe poder apagar o encender'''
+
+
+'''3. HU03: Distribución de dispositivos en el hogar
+Pueda decir cuántas habitaciones tiene el hogar y qué habitaciones son.
+Pueda decir que dispositivos hay en el hogar.
+Pueda añadir algún dispositivo.
+Pueda quitar algún dispositivo.
+Pueda modificar algún dispositivo.
+Pueda saber la cantidad de dispositivos que hay tanto en el hogar como en cada habitación.
+Pueda identificar el dispositivo que está en cada sitio.'''
