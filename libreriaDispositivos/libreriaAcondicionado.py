58381252483948811ac15aee82716b3419789764
Lolo <mrg00130@red.ujaen.es>
2025-10-25 19:32:47 +0200

    Primera version de oteintada a objetos la parte de poner en clase las bobillas y aires

--- libreriaDispositivos/libreriaAcondicionado.py
+++ libreriaDispositivos/libreriaAcondicionado.py
@@ -1,29 +1,25 @@
+class AireAcondicionado:
 
-def creaAireAcondicionado(descripcion = "Aire salon", estado = True, temperatura = 27):
-    aire = { "descripcion": descripcion, "estado" : estado, "temperatura" : temperatura }
-    return aire
+    def __init__(self, descripcion="Aire Salón", estado=False, temperatura=24):
+        self.__descripcion = descripcion
+        self.__estado = estado
+        self.__temperatura = temperatura
 
-def imprimirAire(aire):
-    print("---------------------------------------")
-    if (aire["estado"]):
-        print("El aire acondicionado esta encendido")
-    else:
-        print("El aire acondicionado esta apagado")
-    print("La temperatura es de ", aire["temperatura"])
+    def encender(self):
+        self.__estado = True
 
-    print("---------------------------------------")
+    def apagar(self):
+        self.__estado = False
 
-
-def encenderAire(aire):
-    aire["estado"] = True
-
-def apagarAire(aire):
-    aire["estado"] = False
-def cambiarTemperatura(aire):
-    print("La temperatura es de ", aire["temperatura"])
-    t2 = input("A que temperatura lo quieres ")
-    aire["temperatura"] = t2
-    print("La temperatura es de  --->", aire["temperatura"])
+    def cambiar_temperatura(self, nueva_temp):
+        self.__temperatura = nueva_temp
 
 
+    def get_estado(self):
+        return self.__estado
 
+    def get_temperatura(self):
+        return self.__temperatura
+
+    def get_descripcion(self):
+        return self.__descripcion
--- libreriaDispositivos/libreriaBombilla.py
+++ libreriaDispositivos/libreriaBombilla.py
@@ -1,72 +1,34 @@
-def creaBombilla(tipo="lampara", estado=True, intensidad=100, color=(255, 255, 255)):
-    """Crea un diccionario que representa una bombilla."""
-    bombilla = {
-        "tipo": tipo,
-        "estado": estado,
-        "intensidad": intensidad,
-        "color": color
-    }
-    return bombilla
+class Bombilla:
 
-def imprimirBombilla(bombilla):
-    print("---------------------------------------")
-    if (bombilla["estado"]):
-        print("Bombilla encendida")
-    else:
-        print("Bombilla apagada")
-    print("Nivel de intensidad:", bombilla["intensidad"])
-    print("Color:", bombilla["color"])
-    print("---------------------------------------")
-def encenderBombilla(bombilla):
-    bombilla["estado"] = True
+    def __init__(self, tipo="lámpara", estado=False, intensidad=100, color=(255, 255, 255)):
+        self.__tipo = tipo
+        self.__estado = estado  # False = apagada, True = encendida
+        self.__intensidad = intensidad
+        self.__color = color
 
-def apagarBombilla(bombilla):
-    bombilla["estado"] = False
-def cambiarIntensidad(bombilla):
-    print("La intensidad es de ", bombilla ["lumens"])
-    i2 = input("A que intensidad quieres ")
-    bombilla["intensidad"] = i2
-    print("La intensidad es de  --->", bombilla["intensidad"])
+    def encender(self):
+        self.__estado = True
 
-def cambiarColor(bombilla):
+    def apagar(self):
 
-    color2 = input("elige un color de los siguientes: blanco, rojo, verde, azul, amarillo, cian, magenta. ")
-    color_minusculas = color2.lower()
-    if color_minusculas == "blanco":
-        print("El color cambiado a --->", bombilla["color"])
+        self.__estado = False
 
-    elif color_minusculas == "rojo":
-        bombilla["color"] = (255, 0, 0)
-        print("El color cambiado a --->", bombilla["color"])
+    def cambiar_intensidad(self, nueva_intensidad):
+        if 0 <= nueva_intensidad <= 100:
+            self.__intensidad = nueva_intensidad
 
-    elif color_minusculas == "verde":
-        bombilla["color"] = (0, 255, 0)
-        print("El color cambiado a --->", bombilla["color"])
-
-    elif color_minusculas == "azul":
-        bombilla["color"] = (0, 0, 255)
-        print("El color cambiado a --->", bombilla["color"])
-
-    elif color_minusculas == "amarillo":
-        bombilla["color"] = (255, 255, 0)
-        print("El color cambiado a --->", bombilla["color"])
-
-    elif color_minusculas == "cian":
-        bombilla["color"] = (0, 255, 255)
-        print("El color cambiado a --->", bombilla["color"])
-
-    elif color_minusculas == "magenta":
-        bombilla["color"] = (255, 0, 255)
-        print("El color cambiado a --->", bombilla["color"])
-
-    else:
-
-        print(f"El color '{color_minusculas}' no es una opción válida.")
-        cambiarColor(bombilla)
+    def cambiar_color(self, nuevo_color):
+        self.__color = nuevo_color
 
 
+    def get_estado(self):
+        return self.__estado
 
+    def get_intensidad(self):
+        return self.__intensidad
 
+    def get_color(self):
+        return self.__color
 
-
-
+    def get_tipo(self):
+        return self.__tipo
