57f965288993a547da2102f7c57f1b53454e2c3f
Lolo <mrg00130@red.ujaen.es>
2025-11-22 17:48:49 +0100

    Entrega 4 meto el dispostivo y la bombilla

--- libreriaDispositivos/libreriaBombilla.py
+++ libreriaDispositivos/libreriaBombilla.py
@@ -1,53 +1,44 @@
 from libreriaDispositivos.libreriaProgramador import Programador
+from libreriaDispositivos.dispositivo import Dispositivo
 
-class Bombilla:
+
+class Bombilla(Dispositivo):
     __contador = 0
 
-    def __init__(self, tipo="lámpara", estado=False, intensidad=100, color=(255, 255, 255)):
+    def __init__(self, tipo="Lámpara", estado=False, intensidad=100, color=(255, 255, 255)):
+        super().__init__(nombre=tipo, nivel_inicial=intensidad, min_val=0, max_val=100, estado=estado)
+
         Bombilla.__contador += 1
         self.__id = f"bombilla{Bombilla.__contador}"
-        self.__tipo = tipo
-        self.__estado = estado
-        self.__intensidad = intensidad
         self.__color = color
         self.__programador = None
-
-    def encender(self):
-        self.__estado = True
-
-    def apagar(self):
-        self.__estado = False
-
-    def cambiar_intensidad(self, nueva_intensidad):
-        if 0 <= nueva_intensidad <= 100:
-            self.__intensidad = nueva_intensidad
 
     def cambiar_color(self, nuevo_color):
         self.__color = nuevo_color
 
-    def set_programador(self, programador_obj):
-        if isinstance(programador_obj, Programador):
-            self.__programador = programador_obj
-        else:
-            print("Error: El objeto no es un Programador válido.")
+    def cambiar_intensidad(self, valor_absoluto):
 
-    def get_programador(self):
-        return self.__programador
+        diferencia = valor_absoluto - self._nivelIntensidad
+        if diferencia > 0:
+            self.aumentarIntensidad(diferencia)
+        elif diferencia < 0:
+            self.disminuirIntensidad(abs(diferencia))
 
     def get_id(self):
         return self.__id
 
-    def get_estado(self):
-        return self.__estado
-        
-    def get_tipo(self):
-        return self.__tipo
-
-    
     def get_intensidad(self):
-        """Devuelve la intensidad actual."""
-        return self.__intensidad
-        
+        return self.get_nivel()
+
     def get_color(self):
-        """Devuelve el color actual."""
         return self.__color
+
+    def get_tipo(self):
+        return self.get_nombre()
+
+    def set_programador(self, programador_obj):
+        if isinstance(programador_obj, Programador):
+            self.__programador = programador_obj
+
+    def get_programador(self):
+        return self.__programador
